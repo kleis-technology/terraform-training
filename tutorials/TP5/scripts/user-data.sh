cf145fe7d8a26dcc41acddb8e5dde833c5db9107
jst <jst@kleis.ch>
2021-05-19 00:09:22 +0200

    replace python http.server by nginx for VM demo webserver

--- tutorials/TP5/scripts/user-data.sh
+++ tutorials/TP5/scripts/user-data.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 set -e
 
-mkdir -p /tmp/web
-cd /tmp/web
+apt update
+apt -y install nginx
 
-if [ "${server_name}" = "mighty_panda" ]; then
-  echo "<h1>Roar!</h1>" > index.html
-elif [ "${server_name}" = "giant_owl" ]; then
-  echo "<h1>Skriiii, from above!</h1>" > index.html
-elif [ "${server_name}" = "cute_beaver" ]; then
-  echo "<h1>Nomnomnom, hey there. I'm a beaver!</h1>" > index.html
+if [[ "${server_name}" == "mighty_panda" ]]; then
+  echo "<h1>Roar!</h1>"
+elif [[ "${server_name}" == "giant_owl" ]]; then
+  echo "<h1>Skriiii, from above!</h1>"
+elif [[ "${server_name}" == "cute_beaver" ]]; then
+  echo "<h1>Nomnomnom, hey there. I'm a beaver!</h1>"
 else
-  echo "<h1>Hello world!<h1>" > index.html
-fi
+  echo "<h1>Hello world!<h1>"
+fi >/var/www/html/index.html
 
-echo "<h2>My name is ${server_name}.</h2>" >> index.html
-
-nohup python3 -m http.server ${server_port} --directory /tmp/web
-
+echo "<h2>My name is ${server_name}.</h2>" >>/var/www/html/index.html
